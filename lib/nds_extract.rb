b2940a1e6825c69fe3edc9194a82978ea67c41a3
Carl Parm <carlparm@gmail.com>
2020-01-22 19:02:46 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -38,7 +38,7 @@ def total_gross(source)
   grand_total = 0
   director_index = 0
   d_hash = directors_totals(source)
-  pp d_hash
+  pp d_hash[0]
   #while director_index < source.length do
    # director_index += 1
  # end
