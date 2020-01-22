90db3e33a70923c590fb683c33cf497176182251
Carl Parm <carlparm@gmail.com>
2020-01-22 18:49:59 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -35,20 +35,7 @@ def list_of_directors(source)
 end
 
 def total_gross(source)
-  directors = 0
-  result = {}
-  while directors < nds.length do
-    result[nds[directors][:name]] = 0
-    movie = 0
-    total = 0
-    while movie < nds[directors][:movies].length do
-      total += nds[directors][:movies][movie][:worldwide_gross]
-      movie += 1
-    end
-    result[nds[directors][:name]] = total
-  directors += 1
-  end
-return result
+
 
   
   # Write this implementation
